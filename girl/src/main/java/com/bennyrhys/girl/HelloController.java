3d23c586c81593640ccb3d03a131ea8cf2243c98
bennyrhys <bennyrhys@163.com>
2019-12-30 17:26:29 +0800

    @controller+thymeleaf

--- girl/src/main/java/com/bennyrhys/girl/HelloController.java
+++ girl/src/main/java/com/bennyrhys/girl/HelloController.java
@@ -2,11 +2,13 @@ package com.bennyrhys.girl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     //通过注解获取配置:类中的属性
     @Autowired
@@ -16,6 +18,7 @@ public class HelloController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say(){
-        return girlConfig.getCupSize()+girlConfig.getAge();
+//        return girlConfig.getCupSize()+girlConfig.getAge();
+        return "index";
     }
 }
